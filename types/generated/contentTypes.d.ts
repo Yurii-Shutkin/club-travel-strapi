7629e054046ed2c4532c1f765bdc9eaea0fa64ae
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-04-28 12:55:14 +0300

    added discount entrie to categories table

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -481,6 +481,7 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    discount: Schema.Attribute.Integer;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
