7d1c58716d69513649a4bfc4b2d9c259bc7a96ed
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-05-04 13:11:37 +0300

    modified server.js

--- config/server.js
+++ config/server.js
@@ -5,6 +5,9 @@ module.exports = ({ env }) => ({
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
+    settings: {
+      proxy: true,
+    },
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
